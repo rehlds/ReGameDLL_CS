add88203909c41ba83c5e43cff191ad680e80c04
GLoOoccK <155241167+GLoOoccK@users.noreply.github.com>
2025-04-13 16:11:43 -0300

    Update CSPlayer.cpp

--- regamedll/dlls/API/CSPlayer.cpp
+++ regamedll/dlls/API/CSPlayer.cpp
@@ -539,6 +539,11 @@ EXT_FUNC bool CCSPlayer::CheckActivityInGame()
 	return (fabs(deltaYaw) >= 0.1f && fabs(deltaPitch) >= 0.1f);
 }
 
+EXT_FUNC void CCSPlayer::Knockback(CBaseEntity *pAttacker, float flModifier)
+{
+	BasePlayer()->Knockback(pAttacker, flModifier);
+}
+
 void CCSPlayer::ResetVars()
 {
 	m_szModel[0] = '\0';
