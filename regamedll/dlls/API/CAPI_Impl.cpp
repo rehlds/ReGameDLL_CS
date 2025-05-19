092c85a3ad1f2e02482907feebc5bc0d131057de
GLoOoccK <155241167+GLoOoccK@users.noreply.github.com>
2025-05-19 08:48:44 -0300

    Update CAPI_Impl.cpp

--- regamedll/dlls/API/CAPI_Impl.cpp
+++ regamedll/dlls/API/CAPI_Impl.cpp
@@ -338,6 +338,7 @@ GAMEHOOK_REGISTRY(CBasePlayer_Observer_Think);
 GAMEHOOK_REGISTRY(CBasePlayer_RemoveAllItems);
 GAMEHOOK_REGISTRY(CBasePlayer_UpdateStatusBar);
 GAMEHOOK_REGISTRY(CBasePlayer_TakeDamageImpulse);
+GAMEHOOK_REGISTRY(CSGameRules_PlayerGotItem);
 
 int CReGameApi::GetMajorVersion() {
 	return REGAMEDLL_API_VERSION_MAJOR;
